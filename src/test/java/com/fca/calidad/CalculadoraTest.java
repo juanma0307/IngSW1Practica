c869702feda92893b882a683c512da1e42e40a0e
juanma0307 <64173244+juanma0307@users.noreply.github.com>
2020-04-27 13:30:19 -0500

    subiendo proyecto calculadora

--- src/test/java/com/fca/calidad/CalculadoraTest.java
+++ src/test/java/com/fca/calidad/CalculadoraTest.java
@@ -2,43 +2,73 @@ package com.fca.calidad;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class CalculadoraTest {
-	private Calculadora miCalculadora ;
-	
+	private Calculadora miCalculadora;
 	@Before
-	public void setup(){
+	public void setup() {
 		 miCalculadora = new Calculadora();
-	}
 	
-
+}
 	@Test
-	public void sumaPositivosTest() {
-		//Llamar al código
-		float resultadoEsperado = 6;
+	public void sumapositivo() {
+		//llamar al codigo
+		float resultadoesperado=6;
 		float resultadoEjecucion = miCalculadora.suma(2, 4);
-		//Verificar
-		assertThat(resultadoEsperado, equalTo(resultadoEjecucion));
+		//verificar
+		asserThat(resultadoesperado, equalTo(resultadoEjecucion));
+	}
+	@Test
+	public void sumaConceroTest() {
+		//llamar al codigo
+		float resultadoEsperando=4;
+		float resultadoEjecucion = miCalculadora.suma(0,4);
+		//verificar
+		asserThat(resultadoesperado, equalTo(resultadoEjecucion));
 	}
 	
 	@Test
-	public void sumaConCeroTest() {
-		//Llamar al código
-		float resultadoEsperado = 4;
-		float resultadoEjecucion = miCalculadora.suma(0,4);
-	
-		//Verificar
-		assertThat(resultadoEsperado,equalTo(resultadoEjecucion));
+	public void Multiplicacionormal() {
+	//llamar al codigo	
+		float resultadoesperando= 8;
+		float resultadoEjecucion= miCalculadora.multiplica(2,4);
+	//verificar
+		asserThat(resultadoesperado, equalTo(resultadoEjecucion));
+		
+	}
+	@Test
+	public void Multiplicacionconcero() {
+	//llamar al codigo	
+		float resultadoesperando= 0;
+		float resultadoEjecucion= miCalculadora.multiplica(0,4);
+	//verificar
+		asserThat(resultadoesperado, equalTo(resultadoEjecucion));	
+	}
+	@Test
+	public void divisionnormal() {
+	//llamar al codigo	
+		float resultadoesperando= 5;
+		float resultadoEjecucion= miCalculadora.divide(25,5);
+	//verificar
+		asserThat(resultadoesperado, equalTo(resultadoEjecucion));	
 	}
 	
+	@Test
+	public void divisioncondecimal() {
+	//llamar al codigo	
+		double  resultadoesperando= 0.2;
+		float resultadoEjecucion= miCalculadora.divide(5,25);
+	//verificar
+		asserThat(resultadoesperado, equalTo(resultadoEjecucion));	
+	}
 	@After
-	public void tearDown(){
-		System.out.println("Prueba terminada!");
+	public void teardown() {
+		System.out.println("prueba terminada");
 	}
 
 }
