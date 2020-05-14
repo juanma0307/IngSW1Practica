ee849b16d189e4ab416923ad1dab6e3659969a60
“Your <juanmanuel030797@gmail.com>
2020-05-14 04:37:35 -0500

    prueba buena de uady v.2

--- src/test/java/com/fca/calidad/funcionales/UadyVirtualLoginTest.java
+++ src/test/java/com/fca/calidad/funcionales/UadyVirtualLoginTest.java
@@ -17,42 +17,42 @@ public class UadyVirtualLoginTest{
 
   @Before
   public void setUp() throws Exception {
-	  System.setProperty("webdriver.chrome.driver"," /Users/JUANM/Documents/chromedriver.exe");
+	  System.setProperty("webdriver.chrome.driver","/Users/JUANM/Documents/chromedriver.exe");
     driver = new ChromeDriver();
     baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
   @Test
-  adadgagagf
-	  public void testCasoDePruebaFallidaUady() throws Exception {
-		 driver.get("https://www.google.com/search?q=uady+virtual&rlz=1C1CHBF_esMX872MX873&oq=ua&aqs=chrome.0.69i59j69i57j35i39j46l2j0l3.1159j0j8&sourceid=chrome&ie=UTF-8");
-		 driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/a/div")).click();
-		 driver.findElement(By.xpath("//nav[@id='essentialnavbar']/div/div/div/div/div/div/ul/li/a/em")).click();
-		 driver.findElement(By.xpath("//form[@id='login']/div")).click();
-		 driver.findElement(By.id("username")).clear();
-		 driver.findElement(By.id("username")).sendKeys("aadadada");
-		 driver.findElement(By.id("yui_3_17_2_1_1589423028089_39")).click();
-		 driver.findElement(By.id("password")).clear();
-		 driver.findElement(By.id("password")).sendKeys("adadadad");
-		 driver.findElement(By.id("loginbtn")).click();
-	//	assertTextoPresent();
-		
-	    
-  }
-  @Test
-  public void testCasoDePruebaUadyacertada() throws Exception {
-    driver.get("https://www.google.com/search?q=uady+virtual&rlz=1C1CHBF_esMX872MX873&oq=ua&aqs=chrome.0.69i59j69i57j35i39j46l2j0l3.1159j0j8&sourceid=chrome&ie=UTF-8");
-    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/a/h3")).click();
+  public void testCasoDePruebaFallidaUady() throws Exception {
+    driver.get("https://es.uadyvirtual.uady.mx/");
     driver.findElement(By.linkText("Ingresar")).click();
     driver.findElement(By.id("username")).click();
-    driver.findElement(By.id("username")).sendKeys("a16111353");
-    driver.findElement(By.id("yui_3_17_2_1_1589417984910_38")).click();
-    driver.findElement(By.id("yui_3_17_2_1_1589417984910_36")).click();
-    driver.findElement(By.id("password")).sendKeys("");;
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("adsw");
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("adwdw");
     driver.findElement(By.id("loginbtn")).click();
-  // assertTextoPresent = (By.id("page-my-index"));	
+    driver.findElement(By.xpath("//section[@id='region-main']/div/div/div/div/div")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Datos erróneos\\. Por favor, inténtelo otra vez\\.[\\s\\S]*$"));
   }
-
+	  
+  @Test
+  public void testPruebaExitosaUady() throws Exception {
+    driver.get("https://es.uadyvirtual.uady.mx/");
+   // driver.findElement(By.xpath("//div[@id='rso']/div/div/div/a/h3")).click();
+    driver.findElement(By.xpath("//nav[@id='essentialnavbar']/div/div/div/div/div/div/ul/li/a/em")).click();
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("a16111353");
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("Manolito12345");
+    driver.findElement(By.id("loginbtn")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Administración de la Calidad del Software - E[\\s\\S]*$"));
+  }
   @After
   public void tearDown() throws Exception {
     driver.quit();
